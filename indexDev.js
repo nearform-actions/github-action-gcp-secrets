4174d7c8fa49b591c76416ea4330f6256e74e6a4
Davide Fiorello <davide@codeflyer.com>
2023-03-21 19:16:19 +0100

    feat: cleanup

--- indexDev.js
+++ indexDev.js
@@ -3,7 +3,6 @@ const upsertSecret = require('./src/upsert-secret')
 async function run() {
   try {
     const projectId = 'slack-kb-chatgpt-responder'
-    // const secretName = `projects/slack-kb-chatgpt-responder/secrets/test-action2`
     const secretName = `test-action`
     const secretValue = `new-value`
     console.log(
