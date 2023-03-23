13862803c3004e1d6f28cda6598f355edfa7db57
Davide Fiorello <davide@codeflyer.com>
2023-03-23 16:54:02 +0100

    feat: add tesct scripts

---
+++ scripts/delete-secret.js
@@ -0,0 +1,40 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+
+const secretManagerServiceClient = new SecretManagerServiceClient()
+
+async function deleteSecret() {
+  if (process.argv.length < 4) {
+    console.error('Args not valid')
+    console.error('Usage: node scripts/delete-secret.js PROJECT_ID SECRET')
+  }
+
+  const projectId = process.argv[2]
+  const secretName = process.argv[3]
+
+  const fullSecretName = `projects/${projectId}/secrets/${secretName}`
+
+  try {
+    console.log(`[${fullSecretName}] Delete secret if exists.`)
+
+    await secretManagerServiceClient.getSecret({
+      name: fullSecretName
+    })
+
+    await secretManagerServiceClient.deleteSecret({
+      name: fullSecretName
+    })
+
+    console.log(`[${fullSecretName}] Secret deleted.`)
+    process.exit(0)
+  } catch (e) {
+    if (e.message.startsWith('5 NOT_FOUND:')) {
+      console.log(`[${fullSecretName}] Secret not found, ne need to delete.`)
+      process.exit(0)
+    }
+
+    console.error(e.message)
+    process.exit(1)
+  }
+}
+
+deleteSecret()
---
+++ scripts/verify-secret.js
@@ -0,0 +1,55 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+
+const secretManagerServiceClient = new SecretManagerServiceClient()
+
+async function verifySecret() {
+  if (process.argv.length < 5) {
+    console.error('Args not valid')
+    console.error(
+      'Usage: node scripts/verify-secret.js PROJECT_ID SECRET VERSION'
+    )
+  }
+
+  const projectId = process.argv[2]
+  const secretName = process.argv[3]
+  const expectedVersion = process.argv[4]
+
+  const fullSecretName = `projects/${projectId}/secrets/${secretName}`
+
+  try {
+    console.log(`[${fullSecretName}]: Verify latest secret version.`)
+
+    const [version] = await secretManagerServiceClient.accessSecretVersion({
+      name: `${fullSecretName}/versions/latest`
+    })
+
+    const versionFound = version.name.split('/').pop()
+
+    if (versionFound !== expectedVersion) {
+      throw new Error(
+        `[${fullSecretName}]: Last secret wrong version. Expected: ${expectedVersion} Found: ${versionFound}.`
+      )
+    }
+
+    const storedSecret = version.payload.data.toString()
+    if (process.argv[5] && storedSecret !== process.argv[5]) {
+      throw new Error(
+        `[${fullSecretName}]: The value of the secret is not wath was expected.`
+      )
+    }
+
+    console.log(`[${fullSecretName}]: The secret version is correct`)
+
+    process.exit(0)
+  } catch (e) {
+    if (e.message.startsWith('5 NOT_FOUND:')) {
+      console.log(`[${fullSecretName}] Secret not found.`)
+    } else {
+      console.error(e.message)
+    }
+
+    process.exit(1)
+  }
+}
+
+verifySecret()
